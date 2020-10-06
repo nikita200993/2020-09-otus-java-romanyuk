8ef5a319c5ab8aa4d219434dc3f5ed74183287bd
NikitaRR <nikita200993@mail.ru>
2020-10-06 13:55:46 +0300

    invoke guava class

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -1,8 +1,14 @@
 package ru.otus;
 
+import com.google.common.collect.HashBiMap;
+
+import java.util.Map;
+
 public class HelloOtus {
 
     public static void main(String[] args) {
-        System.out.println("Hello Otus");
+        final Map<String, String> senselessMap = HashBiMap.create();
+        senselessMap.put("message", "Hello Otus");
+        System.out.println(senselessMap.get("message"));
     }
 }
