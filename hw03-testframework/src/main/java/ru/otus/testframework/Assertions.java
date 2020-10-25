31095ed758ab814e687d440dec45a80427275f0c
NikitaRR <nikita200993@mail.ru>
2020-10-25 18:11:29 +0300

    add assertions

---
+++ hw03-testframework/src/main/java/ru/otus/testframework/Assertions.java
@@ -0,0 +1,18 @@
+package ru.otus.testframework;
+
+import java.util.Objects;
+
+public class Assertions {
+
+    public static void assertEquals(final Object expected, final Object actual) {
+        if (!Objects.equals(expected, actual)) {
+            throw new AssertionFailure(
+                    String.format(
+                            "Objects are not equals:%nExpected = %s%nActual = %s",
+                            expected,
+                            actual
+                    )
+            );
+        }
+    }
+}
