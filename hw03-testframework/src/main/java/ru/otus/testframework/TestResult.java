935a22e811167ea216d82e58f543e68c48a55ea1
NikitaRR <nikita200993@mail.ru>
2020-10-25 18:17:18 +0300

    add class

---
+++ hw03-testframework/src/main/java/ru/otus/testframework/TestResult.java
@@ -0,0 +1,74 @@
+package ru.otus.testframework;
+
+import ru.otus.utils.Contracts;
+
+class TestResult {
+
+    private final String testName;
+    private boolean failure = false;
+    private boolean error = false;
+    private Exception exception;
+
+    TestResult(final String testName)
+    {
+        Contracts.requireNonNullArgument(testName);
+
+        this.testName = testName;
+    }
+
+    boolean isSuccess()
+    {
+        return !failure && !error;
+    }
+
+    boolean isFailure()
+    {
+        return failure;
+    }
+
+    boolean isError()
+    {
+        return error;
+    }
+
+    String getTestName()
+    {
+        return testName;
+    }
+
+    void assertionFailed(final AssertionFailure exception)
+    {
+        forbidAnyFlagIsRaised();
+        forbidExceptionIsSet();
+        this.failure = true;
+        this.exception = exception;
+    }
+
+    void addException(final Exception exception)
+    {
+        if (!error && !failure) {
+            forbidExceptionIsSet();
+            this.error = true;
+            this.exception = exception;
+        } else {
+            Contracts.requireNonNullArgument(exception);
+            this.exception.addSuppressed(exception);
+        }
+    }
+
+    Exception getException()
+    {
+        return exception;
+    }
+
+    private void forbidAnyFlagIsRaised()
+    {
+        Contracts.forbidThat(error);
+        Contracts.forbidThat(failure);
+    }
+
+    private void forbidExceptionIsSet()
+    {
+        Contracts.requireThat(exception == null);
+    }
+}
