5a57aad9e33a5c8edcfb6988fcf70b31a201149c
NikitaRR <nikita200993@mail.ru>
2020-12-05 21:42:49 +0300

    add exception

---
+++ hw08-json/src/main/java/ru/otus/json/SerializationInvalidClassPassed.java
@@ -0,0 +1,8 @@
+package ru.otus.json;
+
+public class SerializationInvalidClassPassed extends RuntimeException {
+
+    public SerializationInvalidClassPassed(final String message) {
+        super(message);
+    }
+}
