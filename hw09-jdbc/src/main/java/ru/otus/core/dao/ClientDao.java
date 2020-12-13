13941dc10f99067efe04efa8224826ee4f91249f
NikitaRR <nikita200993@mail.ru>
2020-12-13 22:04:02 +0300

    core

---
+++ hw09-jdbc/src/main/java/ru/otus/core/dao/ClientDao.java
@@ -0,0 +1,18 @@
+package ru.otus.core.dao;
+
+import ru.otus.core.model.Client;
+import ru.otus.core.sessionmanager.SessionManager;
+
+import java.util.Optional;
+
+public interface ClientDao {
+    Optional<Client> findById(long id);
+    //List<Client> findAll();
+
+    long insert(Client client);
+
+    //void update(Client client);
+    //long insertOrUpdate(Client client);
+
+    SessionManager getSessionManager();
+}
