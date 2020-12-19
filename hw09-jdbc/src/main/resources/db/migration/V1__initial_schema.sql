6b372194ef2096d8deec37bb9ccc42313a2cd63f
NikitaRR <nikita200993@mail.ru>
2020-12-19 13:59:26 +0300

    migration script

--- hw09-jdbc/src/main/resources/db/migration/V1__initial_schema.sql
+++ hw09-jdbc/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,11 +1,14 @@
-create table test
-(
-    id   int,
-    name varchar(50)
-);
 create table client
 (
     id   bigserial not null primary key,
-    name varchar(50)
+    name varchar(50),
+    age  integer
+);
+
+create table account
+(
+    no varchar(50) not null primary key,
+    type varchar(50) not null,
+    rest real not null
 );
 
