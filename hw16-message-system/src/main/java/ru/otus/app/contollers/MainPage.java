cbab16c2632a26ed3413e1e9c810cf711e9c525f
NikitaRR <nikita200993@mail.ru>
2021-03-01 21:07:59 +0300

    main page

---
+++ hw16-message-system/src/main/java/ru/otus/app/contollers/MainPage.java
@@ -0,0 +1,23 @@
+package ru.otus.app.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.otus.app.services.UserService;
+import ru.otus.utils.Contracts;
+
+@Controller
+public class MainPage {
+
+    private final UserService userService;
+
+    public MainPage(final UserService userService) {
+        this.userService = Contracts.ensureNonNullArgument(userService);
+    }
+
+    @GetMapping({"/"})
+    public String usersPage(final Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "users";
+    }
+}
