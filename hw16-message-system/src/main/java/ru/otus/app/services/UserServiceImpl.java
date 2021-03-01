a4315f312342d01c72a83e76500cbfec9b75e800
NikitaRR <nikita200993@mail.ru>
2021-03-01 19:26:06 +0300

    adapt to changed interface

--- hw16-message-system/src/main/java/ru/otus/app/services/UserServiceImpl.java
+++ hw16-message-system/src/main/java/ru/otus/app/services/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.otus.app.services;
 
 import ru.otus.app.dao.UserDao;
+import ru.otus.app.dto.UserDto;
 import ru.otus.app.model.User;
 import ru.otus.app.sessionmanager.SessionManager;
 import ru.otus.utils.Contracts;
@@ -8,6 +9,7 @@ import ru.otus.utils.Contracts;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class UserServiceImpl implements UserService {
 
@@ -18,27 +20,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<User> findAll() {
-        return doInTransaction(userDao::findAll);
+    public List<UserDto> findAll() {
+        return doInTransaction(userDao::findAll).stream()
+                .map(User::toUserDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Optional<User> findById(long id) {
-        return doInTransaction(() -> userDao.findById(id));
+    public Optional<UserDto> findById(long id) {
+        return doInTransaction(() -> userDao.findById(id))
+                .map(User::toUserDto);
     }
 
     @Override
-    public Optional<User> findByLogin(final String login) {
+    public Optional<UserDto> findByLogin(final String login) {
         Contracts.requireNonNullArgument(login);
 
-        return doInTransaction(() -> userDao.findByLogin(login));
+        return doInTransaction(() -> userDao.findByLogin(login))
+                .map(User::toUserDto);
     }
 
     @Override
-    public long insert(User user) {
-        Contracts.requireNonNullArgument(user);
+    public long insert(final UserDto userDto) {
+        Contracts.requireNonNullArgument(userDto);
 
-        return doInTransaction(() -> userDao.insert(user));
+        return doInTransaction(() -> userDao.insert(userDto.toUser()));
     }
 
     private <V> V doInTransaction(final Supplier<V> supplier) {
