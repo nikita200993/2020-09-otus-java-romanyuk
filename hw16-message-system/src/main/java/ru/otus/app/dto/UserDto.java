bd2e24810b0f18f7d7cafd62ee9aa90928063c70
NikitaRR <nikita200993@mail.ru>
2021-03-20 21:27:27 +0300

    dto and jpa entities refined

--- hw16-message-system/src/main/java/ru/otus/app/dto/UserDto.java
+++ hw16-message-system/src/main/java/ru/otus/app/dto/UserDto.java
@@ -1,19 +1,30 @@
 package ru.otus.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ru.otus.app.model.Role;
 import ru.otus.app.model.User;
+import ru.otus.messagesystem.client.ResultDataType;
 import ru.otus.utils.Contracts;
 
-public class UserDto {
+import java.io.Serializable;
+
+public class UserDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final String login;
     private final String password;
     private final String role;
 
-    public UserDto(final String login, final String password, final Role role) {
+    @JsonCreator
+    public UserDto(
+            @JsonProperty("login") final String login,
+            @JsonProperty("password") final String password,
+            @JsonProperty("role") final String role) {
         this.login = Contracts.ensureNonNullArgument(login);
         this.password = Contracts.ensureNonNullArgument(password);
-        this.role = Contracts.ensureNonNullArgument(role.getRoleName());
+        this.role = Contracts.ensureNonNullArgument(role);
         Role.isValidRole(this.role);
     }
 
