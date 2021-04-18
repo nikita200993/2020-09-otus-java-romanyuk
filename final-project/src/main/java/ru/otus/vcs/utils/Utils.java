901597cbf96b102fb0d0672e1b33ddfc4cc7984e
NikitaRR <n.romanyuk@rt-solar.ru>
2021-04-18 18:29:53 +0300

    utils

--- final-project/src/main/java/ru/otus/vcs/utils/Utils.java
+++ final-project/src/main/java/ru/otus/vcs/utils/Utils.java
@@ -1,7 +1,13 @@
 package ru.otus.vcs.utils;
 
+import ru.otus.utils.Contracts;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterOutputStream;
 
@@ -33,5 +39,50 @@ public class Utils {
         } catch (final IOException ex) {
             throw new IllegalStateException("Here can't be io error. Writing toy byte array output stream", ex);
         }
+    }
+
+    public static byte[] concat(final byte[] first, final byte[] second) {
+        Contracts.requireNonNullArgument(first);
+        Contracts.requireNonNullArgument(second);
+
+        final byte[] concatResult = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, concatResult, first.length, second.length);
+        return concatResult;
+    }
+
+    public static byte[] concat(final List<byte[]> byteArrays) {
+        Contracts.requireNonNullArgument(byteArrays);
+        final Optional<Integer> totalBytesOpt = byteArrays.stream()
+                .map(byteArr -> byteArr.length)
+                .reduce(Integer::sum);
+        if (totalBytesOpt.isEmpty() || totalBytesOpt.get() == 0) {
+            return new byte[0];
+        }
+        final var result = Arrays.copyOf(byteArrays.get(0), totalBytesOpt.get());
+        var arrayToConcat = byteArrays.get(1);
+        int start = byteArrays.get(0).length;
+        for (int i = 1; i < byteArrays.size(); i++) {
+            System.arraycopy(arrayToConcat, 0, result, start, arrayToConcat.length);
+        }
+        return result;
+    }
+
+    public static int indexOf(final byte[] bytes, final byte searchValue, final int start, final int end) {
+        Contracts.requireNonNullArgument(bytes);
+        Contracts.requireThat(start < bytes.length);
+        Contracts.requireThat(end <= bytes.length);
+
+        for (int pos = start; pos < end; pos++) {
+            if (bytes[pos] == searchValue) {
+                return pos;
+            }
+        }
+        return -1;
+    }
+
+    public static String utf8(final byte[] bytes) {
+        Contracts.requireNonNullArgument(bytes);
+
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 }
