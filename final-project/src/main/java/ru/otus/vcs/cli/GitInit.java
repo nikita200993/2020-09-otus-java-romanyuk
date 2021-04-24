a718f41c8e4614e7487c150091b213a6b2221267
NikitaRR <n.romanyuk@rt-solar.ru>
2021-04-24 12:30:45 +0300

    finish git init

--- final-project/src/main/java/ru/otus/vcs/cli/GitInit.java
+++ final-project/src/main/java/ru/otus/vcs/cli/GitInit.java
@@ -2,18 +2,27 @@ package ru.otus.vcs.cli;
 
 import io.airlift.airline.Arguments;
 import io.airlift.airline.Command;
+import ru.otus.vcs.exception.UserException;
+import ru.otus.vcs.processor.CommandProcessor;
 
 import javax.annotation.Nullable;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 
 @Command(name = "init", description = "Init git repository")
 public class GitInit extends GitCommand {
 
-    @Arguments(description = "path to repository")
+    @Arguments(description = "path to repository", title = "path")
     @Nullable
-    private String path;
+    private String stringPath;
 
     @Override
     public void run() {
-        System.out.println("init command");
+        try {
+            final var path = Path.of(stringPath  == null ? "" : stringPath);
+            new CommandProcessor().init(path);
+        } catch (final InvalidPathException ex) {
+            throw new UserException("Bad path '" + stringPath + "'.");
+        }
     }
 }
