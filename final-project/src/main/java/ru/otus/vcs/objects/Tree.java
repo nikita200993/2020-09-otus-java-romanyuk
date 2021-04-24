dc01e52b93becfd12b37443e7875988ee2a647fa
NikitaRR <n.romanyuk@rt-solar.ru>
2021-04-24 22:51:43 +0300

    rework tree

--- final-project/src/main/java/ru/otus/vcs/objects/Tree.java
+++ final-project/src/main/java/ru/otus/vcs/objects/Tree.java
@@ -1,33 +1,55 @@
 package ru.otus.vcs.objects;
 
 import ru.otus.utils.Contracts;
+import ru.otus.vcs.index.Index;
+import ru.otus.vcs.index.IndexEntry;
+import ru.otus.vcs.path.VCSPath;
+import ru.otus.vcs.ref.Sha1;
+import ru.otus.vcs.repository.GitRepository;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static ru.otus.vcs.utils.Utils.concat;
 
 public class Tree extends GitObject {
 
-    public static final String type = "tree";
-
     private final List<TreeLeaf> leaves;
 
     Tree(final List<TreeLeaf> leaves) {
+        Contracts.requireNonNullArgument(leaves);
+        checkNoRepeatedFileNames(leaves);
+
         this.leaves = Contracts.ensureNonNullArgument(leaves);
     }
 
     public static Tree deserialize(final byte[] entriesList) {
+        Contracts.requireNonNullArgument(entriesList);
+
         int start = 0;
         final var result = new ArrayList<TreeLeaf>();
+        final var uniqueLeaves = new HashSet<TreeLeaf>();
         while (start < entriesList.length) {
             final var leafAndOffset = TreeLeaf.deserialize(entriesList, start);
             result.add(leafAndOffset.first());
+            Contracts.requireThat(uniqueLeaves.add(leafAndOffset.first()), "Repeated file name in tree data");
             start = leafAndOffset.second();
         }
         return new Tree(result);
+    }
+
+    Index index(final GitRepository repository) {
+        Contracts.requireNonNullArgument(repository);
+
+        final var currentDir = VCSPath.root;
+        final var visitedTrees = new HashSet<Sha1>();
+        final var collectedIndexEntries = new ArrayList<IndexEntry>();
+        collectIndexEntries(currentDir, repository, collectedIndexEntries, visitedTrees);
+        return Index.create(collectedIndexEntries);
     }
 
     public List<TreeLeaf> getLeaves() {
@@ -45,6 +67,35 @@ public class Tree extends GitObject {
                 .map(TreeLeaf::serialize)
                 .collect(Collectors.toList())
         );
+    }
+
+    private static void checkNoRepeatedFileNames(final List<TreeLeaf> treeLeaves) {
+        Contracts.requireThat(treeLeaves.size() == new HashSet<>(treeLeaves).size());
+    }
+
+    private void collectIndexEntries(
+            final VCSPath currentDir,
+            final GitRepository repository,
+            final List<IndexEntry> indexEntries,
+            final Set<Sha1> visitedTress) {
+        Contracts.requireThat(visitedTress.add(sha1()), "Cycle in tree O_o.");
+
+        for (final var leaf : leaves) {
+            if (leaf.getType() == FileType.Regular) {
+                indexEntries.add(
+                        IndexEntry.newNormalEntry(currentDir.resolve(leaf.getFileName()), leaf.getSha())
+                );
+            } else {
+                final var gitObject = repository.readGitObjectOrThrowIfAbsent(leaf.getSha());
+                Contracts.requireThat(gitObject instanceof Tree);
+                ((Tree) gitObject).collectIndexEntries(
+                        currentDir.resolve(leaf.getFileName()),
+                        repository,
+                        indexEntries,
+                        visitedTress
+                );
+            }
+        }
     }
 
     @Override
--- final-project/src/main/java/ru/otus/vcs/objects/TreeLeaf.java
+++ final-project/src/main/java/ru/otus/vcs/objects/TreeLeaf.java
@@ -1,36 +1,36 @@
 package ru.otus.vcs.objects;
 
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
 import ru.otus.utils.Contracts;
-import ru.otus.vcs.exception.InnerException;
+import ru.otus.vcs.path.VCSFileName;
+import ru.otus.vcs.ref.Sha1;
 import ru.otus.vcs.utils.Tuple2;
 
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
-import static ru.otus.vcs.utils.Utils.*;
+import static ru.otus.vcs.utils.Utils.indexOf;
+import static ru.otus.vcs.utils.Utils.utf8;
 
 
 public class TreeLeaf {
 
-    private final FileMode mode;
-    private final Path path;
-    private final String sha;
+    private final FileType type;
+    private final VCSFileName fileName;
+    private final Sha1 sha;
 
-    public TreeLeaf(final FileMode mode, final Path path, final String sha) {
-        this.mode = Contracts.ensureNonNullArgument(mode);
-        this.path = Contracts.ensureNonNullArgument(path);
+    public TreeLeaf(final FileType type, final VCSFileName fileName, final Sha1 sha) {
+        this.type = Contracts.ensureNonNullArgument(type);
+        this.fileName = Contracts.ensureNonNullArgument(fileName);
         this.sha = Contracts.ensureNonNullArgument(sha);
     }
 
     public static Tuple2<TreeLeaf, Integer> deserialize(final byte[] entriesList, final int start) {
-        final var fileModeAndNewOffset = deserializeFileMode(entriesList, start);
-        final var pathAndNewOffset = deserializePath(entriesList, fileModeAndNewOffset.second());
+        final var fileModeAndNewOffset = deserializeFileType(entriesList, start);
+        final var pathAndNewOffset = deserializeFileName(
+                entriesList,
+                fileModeAndNewOffset.second()
+        );
         final var shaAndNewOffset = deserializeSha(entriesList, pathAndNewOffset.second());
         return new Tuple2<>(
                 new TreeLeaf(
@@ -42,60 +42,54 @@ public class TreeLeaf {
         );
     }
 
-    public FileMode getMode() {
-        return mode;
+    static TreeLeaf deserialize(final byte[] treeLeafData) {
+        Contracts.requireNonNullArgument(treeLeafData);
+
+        return deserialize(treeLeafData, 0).first();
     }
 
-    public Path getPath() {
-        return path;
+    public FileType getType() {
+        return type;
     }
 
-    public String getSha() {
+    public VCSFileName getFileName() {
+        return fileName;
+    }
+
+    public Sha1 getSha() {
         return sha;
     }
 
     public byte[] serialize() {
-        try {
-            final byte[] partOne = (Integer.toString(mode.asIntFlags())
-                    + ' '
-                    + path.toString()
-                    + (char) 0).getBytes(StandardCharsets.UTF_8);
-            final byte[] binarySha = Hex.decodeHex(sha);
-            return concat(partOne, binarySha);
-        } catch (final DecoderException ex) {
-            throw new InnerException("Can't convert sha1 hex string to binary.", ex);
-        }
+        final var content = type.getFileTypeName()
+                + ' '
+                + fileName.getName()
+                + (char) 0
+                + sha.getHexString();
+        return content.getBytes(StandardCharsets.UTF_8);
     }
 
-    static TreeLeaf deserialize(final byte[] data) {
-        return deserialize(data, 0).first();
-    }
-
-    private static Tuple2<FileMode, Integer> deserializeFileMode(final byte[] entriesList, final int start) {
+    private static Tuple2<FileType, Integer> deserializeFileType(final byte[] entriesList, final int start) {
         final int end = indexOf(entriesList, (byte) ' ', start, entriesList.length);
-        if (end == -1) {
-            throw new InnerException("Can't deserialize file mode. Failed to find 'space'");
-        }
-        final var intString = utf8(Arrays.copyOfRange(entriesList, start, end));
-        final int flags = Integer.parseInt(intString);
-        return new Tuple2<>(FileMode.fromFlags(flags), end + 1);
+        Contracts.requireThat(end != - 1, "No whitespace in tree leaf data.");
+        final var fileTypeName = utf8(Arrays.copyOfRange(entriesList, start, end));
+        Contracts.requireThat(FileType.isValidFileTypeName(fileTypeName), "Bad file type name " + fileTypeName);
+        return new Tuple2<>(FileType.fromFileTypeName(fileTypeName), end + 1);
     }
 
-    private static Tuple2<Path, Integer> deserializePath(final byte[] entriesList, final int start) {
+    private static Tuple2<VCSFileName, Integer> deserializeFileName(final byte[] entriesList, final int start) {
         final int end = indexOf(entriesList, (byte) 0, start, entriesList.length);
-        if (end == -1) {
-            throw new InnerException("Can't deserialize path. Failed to find null byte");
-        }
-        final String path = utf8(Arrays.copyOfRange(entriesList, start, end));
-        return new Tuple2<>(Path.of(path), end + 1);
+        Contracts.requireThat(end != - 1, "No null byte in tree leaf data.");
+        final String fileName = utf8(Arrays.copyOfRange(entriesList, start, end));
+        Contracts.requireThat(VCSFileName.isValidVCSFileName(fileName), "Bad file name.");
+        return new Tuple2<>(VCSFileName.create(fileName), end + 1);
     }
 
-    private static Tuple2<String, Integer> deserializeSha(final byte[] entriesList, final int start) {
-        if (start + 20 > entriesList.length) {
-            throw new InnerException("Can't deserialize sha. There is no available 20 bytes in array.");
-        }
-        final String sha = new String(Hex.encodeHex(entriesList, start, 20, true));
-        return new Tuple2<>(sha, start + 20);
+    private static Tuple2<Sha1, Integer> deserializeSha(final byte[] entriesList, final int start) {
+        Contracts.requireThat(start + 40 <= entriesList.length, "Bad tree leaf data. Bad sha.");
+        final var shaHex = utf8(Arrays.copyOfRange(entriesList, start, start + 40));
+        Contracts.requireThat(Sha1.isValidSha1HexString(shaHex), "Bad hex string value = " + shaHex + ".");
+        return new Tuple2<>(Sha1.create(shaHex), start + 40);
     }
 
     @Override
@@ -103,11 +97,11 @@ public class TreeLeaf {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TreeLeaf treeLeaf = (TreeLeaf) o;
-        return mode == treeLeaf.mode && path.equals(treeLeaf.path) && sha.equals(treeLeaf.sha);
+        return type == treeLeaf.type && fileName.equals(treeLeaf.fileName) && sha.equals(treeLeaf.sha);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(mode, path, sha);
+        return Objects.hash(type, fileName, sha);
     }
 }
