a6b72656571cfddd47dfad6421f98c1f66caad40
NikitaRR <n.romanyuk@rt-solar.ru>
2021-05-02 18:20:29 +0300

    add method

--- final-project/src/main/java/ru/otus/vcs/newversion/gitrepo/CommitMessage.java
+++ final-project/src/main/java/ru/otus/vcs/newversion/gitrepo/CommitMessage.java
@@ -23,4 +23,20 @@ public class CommitMessage {
         return new CommitMessage(message);
     }
 
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommitMessage that = (CommitMessage) o;
+        return message.equals(that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return message.hashCode();
+    }
 }
