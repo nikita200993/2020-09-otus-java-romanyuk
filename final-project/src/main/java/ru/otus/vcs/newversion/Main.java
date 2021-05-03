d58a958c3c32369c70d5e4f53cbe627f6b438606
NikitaRR <n.romanyuk@rt-solar.ru>
2021-05-03 21:06:17 +0300

    log

--- final-project/src/main/java/ru/otus/vcs/newversion/Main.java
+++ final-project/src/main/java/ru/otus/vcs/newversion/Main.java
@@ -2,6 +2,9 @@ package ru.otus.vcs.newversion;
 
 import io.airlift.airline.Cli;
 import io.airlift.airline.Help;
+import io.airlift.airline.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.vcs.newversion.cli.Add;
 import ru.otus.vcs.newversion.cli.Branch;
 import ru.otus.vcs.newversion.cli.Checkout;
@@ -12,12 +15,20 @@ import ru.otus.vcs.newversion.cli.Status;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String[] args) {
         final var gitCli = Cli.<Runnable>builder("simplegit")
                 .withDescription("version control system")
                 .withDefaultCommand(Help.class)
                 .withCommands(Init.class, Add.class, Remove.class, Branch.class, Checkout.class, Commit.class, Status.class)
                 .build();
-        gitCli.parse(args).run();
+        try {
+            gitCli.parse(args).run();
+        } catch (final ParseException ex) {
+            System.out.println("Bad command line arguments. " + ex.getMessage());
+        } catch (final Throwable throwable) {
+            logger.error("Critical error.", throwable);
+        }
     }
 }
