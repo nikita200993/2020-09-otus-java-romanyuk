4f0b05b49940aa11173a9cc898204290d1d30b0d
NikitaRR <n.romanyuk@rt-solar.ru>
2021-05-03 11:37:45 +0300

    add branch

---
+++ final-project/src/main/java/ru/otus/vcs/newversion/cli/Branch.java
@@ -0,0 +1,16 @@
+package ru.otus.vcs.newversion.cli;
+
+import io.airlift.airline.Arguments;
+import io.airlift.airline.Command;
+
+@Command(name = "branch")
+public class Branch extends GitCommand {
+
+    @Arguments(required = true)
+    private String branchName;
+
+    @Override
+    public void execute() {
+        commandProcessor.branch(branchName);
+    }
+}
